5ea97d3744fb1acf9ee6f9d2135aea5a8fa659d7
Jinxed Nation William Halling <60004450+JinxedNation@users.noreply.github.com>
2023-03-09 15:37:23 +0800

    Client
    Main Prog

---
+++ Client.cpp
@@ -0,0 +1,21 @@
+#include "GameWindow.h"
+
+/*******************
+ * @class   : Client
+ * @brief   : contains main and will load program
+ * @date    : March 2023
+ * @author  : William Halling
+ * @version : 1.0
+ ********************/
+
+
+int main()
+{
+	PixelEngine::GameWindow pixelsEngine(640, 480, "Pixel's World");
+	
+	pixelsEngine.launchGameWindow(650, 640, "Pixels World");
+
+
+	return 0;
+
+}
