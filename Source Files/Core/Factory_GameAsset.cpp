6aa31a8871a1148012cfa3ca66732e3108aef927
Jinxed Nation William Halling <32233703@student.murdoch.edu.au>
2023-03-22 02:58:52 +0800

    Create Factory Method GameAsset class

---
+++ Source Files/Core/Factory_GameAsset.cpp
@@ -0,0 +1,19 @@
+#include "Factory_GameAsset.h"
+
+
+PixelEngine::Object* PixelEngine::Factory_GameAsset::generate_ObjectType(const std::string& temp_ObjectType)
+{
+    /**************************
+    if(temp_ObjectType == "NPC")
+    {
+        return new //NPC();
+    }
+    else 
+        if(temp_ObjectType == "Lake")
+        {
+            return new //Lake();
+        }
+
+    return nullptr;
+    ***********/
+}
