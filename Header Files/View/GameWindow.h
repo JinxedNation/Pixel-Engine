9f0b22e6297ed5d969742778e842fc134510eed2
jasonthepenguin <jasonbotterill@hotmail.com>
2023-03-22 12:20:24 +0800

    Added GLAD for OpenGL 3.2
    Core profile, glad.c and its header files are added. For gl 3.2 and core profile.

--- Header Files/View/GameWindow.h
+++ Header Files/View/GameWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 /******************************************************************************************************************
